a40a45423d6a49deb84986c8e7eddcd9b94a172b
JamKelley22 <jkelley@iastate.edu>
2018-10-20 16:58:05 -0500

    Landing + Stuff

--- frontend/helptrain/src/components/Landing/landing.js
+++ frontend/helptrain/src/components/Landing/landing.js
@@ -1,12 +1,34 @@
 import React from 'react'
 //import PropTypes from 'prop-types'
+import { Link } from "react-router-dom";
+import { history, routes } from '../../history.js'
+
+import largeSVG from '../../media/svg/large_light.svg'
+import smallSVG from '../../media/svg/small_dark.svg'
 
 import './landing.scss'
+
+
 
 const Landing = (props) => {
   return (
     <div className='landingPage'>
-      <h1>Landing</h1>
+
+      <div className='content'>
+        <div className='title'>HelpTrain</div>
+        <p>I am a...</p>
+        <div className='buttons'>
+          <button><span>Student</span></button>
+          <button><span>TA</span></button>
+        </div>
+        <Link to={routes._ABOUT}>What is this?</Link>
+      </div>
+      <div className='svgs'>
+        <img src={largeSVG} className='svg largeRight'/>
+        <img src={smallSVG} className='svg smallRight'/>
+        <img src={largeSVG} className='svg largeLeft'/>
+        <img src={smallSVG} className='svg smallLeft'/>
+      </div>
     </div>
   )
 }
--- frontend/helptrain/src/constants/routes.js
+++ frontend/helptrain/src/constants/routes.js
@@ -1,3 +1,4 @@
 export const _LANDING = '/'
 export const _HOME = '/home'
 export const _LOGIN = '/login'
+export const _ABOUT = '/about'
