46e11724767fb4155f0c9c059d847e8361a16fcd
JamKelley22 <jkelley@iastate.edu>
2018-10-20 22:52:24 -0500

    Fixed overflow

---
+++ frontend/helptrain/src/components/fake/data.js
@@ -0,0 +1,29 @@
+export const queue = [
+  {
+    id: '2',
+    name: 'sam',
+    description: 'test',
+    password: '123456789'//10 alphanumeric
+  }
+]
+
+export const queueMemebrs = [
+  {
+    id: 3,
+    name: 'Barty',
+    question: 'How',
+    QueueID: '1'
+  },
+  {
+    id: 4,
+    name: 'Joe',
+    question: 'What',
+    QueueID: '2'
+  },
+  {
+    id: 5,
+    name: 'Omair',
+    question: 'When',
+    QueueID: '3'
+  },
+]
